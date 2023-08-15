4ea66380d0a5737f5edfbd92875d6c0aa2ffb8e1
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-08-15 20:49:22 -0300

    Add compose navigation lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,9 +52,10 @@ android {
 dependencies {
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
