b09f2138fe7648ecbc161cba69dbe0ada4d9a391
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-08-17 20:25:22 -0300

    Request coarse location when app is opened

--- app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
+++ app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
@@ -1,52 +1,52 @@
 package com.damiandantas.daylighthabits
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.core.content.ContextCompat
+import com.damiandantas.daylighthabits.ui.composables.LocationPermissionDialog
+import com.damiandantas.daylighthabits.ui.theme.AppTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val activityResult = registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            if (!it) finish()
+        }
+
         setContent {
-            MaterialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            val hasLocationPermission by produceState(true) {
+                value = hasLocationPermission()
+            }
 
-                    BottomAppBar {
-                        Text(text = "First text")
+            var showDialog by remember { mutableStateOf(hasLocationPermission) }
 
-                        Text(text = "Second text")
-                    }
+            AppTheme {
+                if (showDialog) {
+                    LocationPermissionDialog(onDismissDialog = {
+                        showDialog = false
+                        finish()
+                    }, onClickButton = {
+                        showDialog = false
+                        activityResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                    })
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MaterialTheme {
-        Greeting("Android")
+    private suspend fun hasLocationPermission() = withContext(Dispatchers.IO) {
+        ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 }
