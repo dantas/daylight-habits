bacdffcf8ae2699325efed7941ab439682cb0857
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-08-19 00:51:05 -0300

    Use LaunchedEffect to query for location permission

--- app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
+++ app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
@@ -6,9 +6,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.produceState
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.core.content.ContextCompat
@@ -28,21 +28,23 @@ class MainActivity : ComponentActivity() {
             }
 
         setContent {
-            val hasLocationPermission by produceState(true) {
-                value = hasLocationPermission()
-            }
-
-            var showDialog by remember { mutableStateOf(!hasLocationPermission) }
-
             AppTheme {
+                var showDialog by remember { mutableStateOf(false) }
+
+                LaunchedEffect(true) {
+                    showDialog = !hasLocationPermission()
+                }
+
                 if (showDialog) {
-                    LocationPermissionDialog(onDismissDialog = {
-                        showDialog = false
-                        finish()
-                    }, onClickButton = {
-                        showDialog = false
-                        activityResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-                    })
+                    LocationPermissionDialog(
+                        onDismissDialog = {
+                            showDialog = false
+                            finish()
+                        }, onClickButton = {
+                            showDialog = false
+                            activityResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                        }
+                    )
                 }
 
                 AppScreen()
