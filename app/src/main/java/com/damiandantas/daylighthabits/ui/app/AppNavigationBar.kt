7b1bdc4336e38ce07c810c9ddf9556df277131b8
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-08-20 00:44:43 -0300

    Simplify AppNavigationBar

--- app/src/main/java/com/damiandantas/daylighthabits/ui/app/AppNavigationBar.kt
+++ app/src/main/java/com/damiandantas/daylighthabits/ui/app/AppNavigationBar.kt
@@ -1,55 +1,58 @@
 package com.damiandantas.daylighthabits.ui.app
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.damiandantas.daylighthabits.R
 import com.damiandantas.daylighthabits.ui.theme.AppTheme
 
 @Composable
-fun AppNavigationBar(selected: Int, onClick: (index: Int) -> Unit) {
-    val selected = remember {
-        mutableStateOf(
-            arrayOf(false, false, false).apply {
-                this[selected] = true
-            }
-        )
-    }
+fun AppNavigationBar(selectedItem: Int, onClick: (index: Int) -> Unit) {
+    val selected = rememberSaveable { mutableStateOf(selectedItem) }
 
     NavigationBar {
-        NavigationBarItem(selected = selected.value[0], onClick = {
-            selected.value = arrayOf(true, false, false)
-            onClick(0)
-        }, icon = {
-            Icon(
-                painterResource(R.drawable.alarm), "Alarm",
-            )
-        }, label = { Text("Alarm") })
+        AppNavigationBarItem(
+            index = 0, icon = R.drawable.alarm, label = "Alarm",
+            selected = selected, onClick = onClick
+        )
 
-        NavigationBarItem(selected = selected.value[1], onClick = {
-            selected.value = arrayOf(false, true, false)
-            onClick(1)
-        }, icon = {
-            Icon(
-                painterResource(R.drawable.routine), "Forecast",
-            )
-        }, label = { Text("Forecast") })
+        AppNavigationBarItem(
+            index = 1, icon = R.drawable.routine, label = "Forecast",
+            selected = selected, onClick = onClick
+        )
 
-        NavigationBarItem(selected = selected.value[2], onClick = {
-            selected.value = arrayOf(false, false, true)
-            onClick(2)
-        }, icon = {
-            Icon(
-                painterResource(R.drawable.instant_mix), "Alarm Settings",
-            )
-        }, label = { Text("Alarm Settings") })
+        AppNavigationBarItem(
+            index = 2, icon = R.drawable.instant_mix, label = "Alarm Settings",
+            selected = selected, onClick = onClick
+        )
     }
+}
+
+@Composable
+fun RowScope.AppNavigationBarItem(
+    index: Int,
+    @DrawableRes icon: Int,
+    label: String,
+    selected: MutableState<Int>,
+    onClick: (index: Int) -> Unit,
+) {
+    NavigationBarItem(selected = selected.value == index, onClick = {
+        selected.value = index
+        onClick(index)
+    }, icon = {
+        Icon(
+            painterResource(icon), label,
+        )
+    }, label = { Text(label) })
 }
 
 @Composable
