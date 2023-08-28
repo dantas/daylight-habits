bbe9fbece2e1f05a0ebb9e60a0666fa18d7cdd4d
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-08-27 23:59:05 -0300

    Add missing import

--- app/src/main/java/com/damiandantas/daylighthabits/ui/screen/AlarmScreen.kt
+++ app/src/main/java/com/damiandantas/daylighthabits/ui/screen/AlarmScreen.kt
@@ -10,7 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
