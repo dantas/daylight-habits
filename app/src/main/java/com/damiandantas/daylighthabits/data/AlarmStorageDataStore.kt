ae8b7fe71acd9c88da671ec056239b0b1662f6f8
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-09-21 00:28:16 -0300

    Implement AlarmStorage using DataStore

---
+++ app/src/main/java/com/damiandantas/daylighthabits/data/AlarmStorageDataStore.kt
@@ -0,0 +1,41 @@
+package com.damiandantas.daylighthabits.data
+
+import androidx.datastore.core.DataStore
+import com.damiandantas.daylighthabits.domain.AlarmStorage
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import java.time.Duration
+
+class AlarmStorageDataStore(
+    private val alarmStorageDataStore: DataStore<AlarmProtoStorage>
+) : AlarmStorage {
+    override suspend fun enable() {
+        alarmStorageDataStore.updateData {
+            it.toBuilder().setIsEnabled(true).build()
+        }
+    }
+
+    override suspend fun disable() {
+        alarmStorageDataStore.updateData {
+            it.toBuilder().setIsEnabled(false).build()
+        }
+    }
+
+    override suspend fun isEnabled(): Boolean =
+        alarmStorageDataStore.data.map {
+            it.isEnabled
+        }.flowOn(Dispatchers.IO).first()
+
+    override suspend fun sleepDuration(): Duration? =
+        alarmStorageDataStore.data.map {
+            if (it.duration == 0L) null else Duration.ofMillis(it.duration)
+        }.flowOn(Dispatchers.IO).first()
+
+    override suspend fun setSleepDuration(duration: Duration) {
+        alarmStorageDataStore.updateData {
+            it.toBuilder().setDuration(duration.toMillis()).build()
+        }
+    }
+}
