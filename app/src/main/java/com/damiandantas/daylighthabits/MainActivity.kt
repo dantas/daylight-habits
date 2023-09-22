91858d616438bccfeb7e42585dfe6e3ac96c8082
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-09-22 09:18:53 -0300

    Simplify code handling location permission

--- app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
+++ app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.damiandantas.daylighthabits
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.damiandantas.daylighthabits.ui.app.AppScreen
 import com.damiandantas.daylighthabits.ui.dialog.LocationPermissionDialog
 import com.damiandantas.daylighthabits.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
@@ -24,45 +25,39 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var showAppScreen by mutableStateOf(false)
+        var hasLocationPermission by mutableStateOf(false)
 
         val activityResult =
             registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-                showAppScreen = it
+                hasLocationPermission = it
                 if (!it) finish()
             }
 
+        lifecycleScope.launch {
+            hasLocationPermission = hasLocationPermission()
+        }
+
         setContent {
             AppTheme {
-                var showDialog by remember { mutableStateOf(false) }
-
-                LaunchedEffect(true) {
-                    showDialog = !hasLocationPermission()
-                }
-
-                if (showDialog) {
+                if (hasLocationPermission) {
+                    AppScreen()
+                } else {
                     LocationPermissionDialog(
                         onDismissDialog = {
-                            showDialog = false
                             finish()
                         }, onClickButton = {
-                            showDialog = false
                             activityResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
                         }
                     )
                 }
-
-                if (showAppScreen) {
-                    AppScreen()
-                }
             }
         }
     }
+}
 
-    private suspend fun hasLocationPermission() = withContext(Dispatchers.IO) {
-        ContextCompat.checkSelfPermission(
-            this@MainActivity,
-            Manifest.permission.ACCESS_COARSE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    }
+private suspend fun Context.hasLocationPermission() = withContext(Dispatchers.IO) {
+    ContextCompat.checkSelfPermission(
+        this@hasLocationPermission,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
 }
