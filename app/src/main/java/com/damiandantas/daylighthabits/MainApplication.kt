5593295c3e24ab3b91c1ad0a2dbb2d040dc933cb
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-10-26 17:35:08 -0300

    Reschedule alerts when app is opened

--- app/src/main/java/com/damiandantas/daylighthabits/MainApplication.kt
+++ app/src/main/java/com/damiandantas/daylighthabits/MainApplication.kt
@@ -1,7 +1,23 @@
 package com.damiandantas.daylighthabits
 
 import android.app.Application
+import com.damiandantas.daylighthabits.modules.alert.domain.AlertRescheduler
+import com.damiandantas.daylighthabits.utils.di.DispatcherDefault
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
-class MainApplication : Application()
+class MainApplication @Inject constructor(
+    private val rescheduler: AlertRescheduler,
+    @DispatcherDefault private val dispatcherDefault: CoroutineScope
+): Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        dispatcherDefault.launch {
+            rescheduler.reschedule()
+        }
+    }
+}
