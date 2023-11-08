5b559e6dc6d1b13d0d3ab5f768234a63e49308c7
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-11-08 14:31:48 -0300

    Simplify verification of permission

--- app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
+++ app/src/main/java/com/damiandantas/daylighthabits/MainActivity.kt
@@ -3,46 +3,47 @@ package com.damiandantas.daylighthabits
 import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.lifecycleScope
 import com.damiandantas.daylighthabits.ui.AppScreen
 import com.damiandantas.daylighthabits.ui.composable.LocationPermissionDialog
 import com.damiandantas.daylighthabits.ui.theme.AppTheme
 import com.damiandantas.daylighthabits.utils.hasLocationPermission
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        var hasLocationPermission by mutableStateOf(false)
+        setContent {
+            var showAppScreen by remember { mutableStateOf(false) }
 
-        val activityResult =
-            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
-                hasLocationPermission = it
-                if (!it) finish()
+            LaunchedEffect(this) {
+                showAppScreen = hasLocationPermission()
             }
 
-        lifecycleScope.launch {
-            hasLocationPermission = hasLocationPermission()
-        }
+            val launcher =
+                rememberLauncherForActivityResult(ActivityResultContracts.RequestPermission()) { enabled ->
+                    showAppScreen = enabled
+                    if (!enabled) finish()
+                }
 
-        setContent {
             AppTheme {
-                if (hasLocationPermission) {
+                if (showAppScreen) {
                     AppScreen()
                 } else {
                     LocationPermissionDialog(
                         onDismissDialog = {
                             finish()
                         }, onClickButton = {
-                            activityResult.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
+                            launcher.launch(Manifest.permission.ACCESS_COARSE_LOCATION)
                         }
                     )
                 }
