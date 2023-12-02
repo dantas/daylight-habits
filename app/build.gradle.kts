af52f53361269c98d32c85a51fa2b2b00e7b2704
Eduardo Damian Dantas <eduardo@damiandantas.com>
2023-12-01 23:04:00 -0300

    Update gradle and activity-compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,7 +92,7 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01")) // Bill of Materials
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
