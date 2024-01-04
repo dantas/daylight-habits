769096fc64193a9253130c4b8c944ae6042fbe1b
Eduardo Damian Dantas <eduardo@damiandantas.com>
2024-01-03 21:00:12 -0300

    Upgrade AGP and several libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,13 +92,13 @@ dependencies {
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01")) // Bill of Materials
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-    implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.30.1")
     implementation(platform("androidx.compose:compose-bom:2023.10.01")) // Bill of Materials
 
